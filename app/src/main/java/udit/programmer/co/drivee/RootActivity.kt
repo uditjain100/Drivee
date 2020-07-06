d909787a3092ee15fe2b88f89c88ac9a377be18a
Udit Jain <jain30udit@gmail.com>
2020-07-06 19:14:39 +0530

    Current Location Work

--- app/src/main/java/udit/programmer/co/drivee/RootActivity.kt
+++ app/src/main/java/udit/programmer/co/drivee/RootActivity.kt
@@ -1,25 +1,34 @@
 package udit.programmer.co.drivee
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.mapbox.android.core.location.LocationEngine
-import com.mapbox.android.core.location.LocationEngineRequest
+import com.mapbox.android.core.location.*
+import com.mapbox.android.core.permissions.PermissionsListener
 import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.api.geocoding.v5.models.CarmenFeature
 import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.camera.CameraPosition
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.location.LocationComponentActivationOptions
+import com.mapbox.mapboxsdk.location.modes.CameraMode
+import com.mapbox.mapboxsdk.location.modes.RenderMode
 import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.mapboxsdk.maps.MapboxMap
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 import com.mapbox.mapboxsdk.maps.Style
+import com.mapbox.mapboxsdk.plugins.traffic.TrafficPlugin
 import com.mapbox.mapboxsdk.style.layers.Layer
 import kotlinx.android.synthetic.main.activity_root.*
+import kotlinx.android.synthetic.main.home_content_main.*
+import java.lang.Exception
+import java.lang.ref.WeakReference
 
-class RootActivity : AppCompatActivity(), OnMapReadyCallback {
+class RootActivity : AppCompatActivity(), OnMapReadyCallback, PermissionsListener {
 
     private lateinit var mapView: MapView
     var mapboxMap: MapboxMap? = null
@@ -46,7 +55,9 @@ class RootActivity : AppCompatActivity(), OnMapReadyCallback {
     private val DEFAULT_MAX_WAIT_TIME = DEFAULT_INTERVAL_IN_MILLISECONDS * 5
 
     private lateinit var request: LocationEngineRequest
+    private var callback = SearchPickActivityLocationCallback(this)
 
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
@@ -59,14 +70,138 @@ class RootActivity : AppCompatActivity(), OnMapReadyCallback {
         mapView.onCreate(savedInstanceState)
         mapView.getMapAsync(this)
 
+        fab_navigate_pick_btn.setOnClickListener {
+            locationEngine!!.requestLocationUpdates(request, callback, mainLooper)
+        }
+
     }
 
     override fun onMapReady(mapboxMap: MapboxMap) {
         this.mapboxMap = mapboxMap
-        mapboxMap.setStyle(Style.OUTDOORS) {
-
+        mapboxMap.setStyle(Style.OUTDOORS) { style ->
+            TrafficPlugin(mapView, mapboxMap, style).setVisibility(true)
+            enableLocationComponent(style)
         }
     }
 
+    @SuppressLint("LogNotTimber", "MissingPermission")
+    private fun enableLocationComponent(it: Style) {
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            val locationComponent = mapboxMap!!.locationComponent
+            locationComponent.activateLocationComponent(
+                LocationComponentActivationOptions.builder(this, it).build()
+            )
+            locationComponent.isLocationComponentEnabled = true
+            locationComponent.cameraMode = CameraMode.TRACKING
+            locationComponent.renderMode = RenderMode.COMPASS
+            initLocationEngine()
+        } else {
+            permissionsManager = PermissionsManager(this)
+            permissionsManager.requestLocationPermissions(this)
+        }
+    }
 
+    @SuppressLint("MissingPermission")
+    private fun initLocationEngine() {
+        locationEngine = LocationEngineProvider.getBestLocationEngine(this);
+
+        request = LocationEngineRequest.Builder(DEFAULT_INTERVAL_IN_MILLISECONDS)
+            .setPriority(LocationEngineRequest.PRIORITY_HIGH_ACCURACY)
+            .setMaxWaitTime(DEFAULT_MAX_WAIT_TIME).build();
+
+        locationEngine!!.requestLocationUpdates(request, callback, mainLooper);
+        locationEngine!!.getLastLocation(callback)
+    }
+
+    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+        Toast.makeText(this, "Explanation Needed", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+        if (granted) mapboxMap!!.getStyle { enableLocationComponent(it) }
+        else Toast.makeText(this, "Permissions Not Granted", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        locationEngine!!.removeLocationUpdates(callback)
+        mapView.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        locationEngine!!.removeLocationUpdates(callback)
+        mapView.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        locationEngine!!.removeLocationUpdates(callback)
+        mapView.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
+
+}
+
+class SearchPickActivityLocationCallback(activity: RootActivity?) :
+    LocationEngineCallback<LocationEngineResult?> {
+    private val activityWeakReference: WeakReference<RootActivity?>?
+
+    init {
+        activityWeakReference = WeakReference(activity)
+    }
+
+    override fun onSuccess(result: LocationEngineResult?) {
+        val activity: RootActivity = activityWeakReference!!.get()!!
+        if (activity != null) {
+            val location = result!!.lastLocation ?: return
+            activity.currentLat = location.latitude
+            activity.currentLng = location.longitude
+            Toast.makeText(
+                activity, "lat : ${activity.currentLat} , lng : ${activity.currentLng}",
+                Toast.LENGTH_SHORT
+            ).show()
+            if (activity.mapboxMap != null && result.lastLocation != null) {
+                activity.mapboxMap!!.locationComponent
+                    .forceLocationUpdate(result.lastLocation)
+                activity.mapboxMap!!.animateCamera(
+                    com.mapbox.mapboxsdk.camera.CameraUpdateFactory.newCameraPosition(
+                        CameraPosition.Builder()
+                            .target(LatLng(activity.currentLat, activity.currentLng)).zoom(14.0)
+                            .build()
+                    ), 4000
+                )
+            }
+        }
+    }
+
+    override fun onFailure(exception: Exception) {
+        val activity: RootActivity = activityWeakReference!!.get()!!
+        if (activity != null) {
+            Toast.makeText(
+                activity, exception.localizedMessage,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+    }
 }
